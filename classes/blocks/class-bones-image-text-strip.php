0e1d81d75383c731034a051b024c97e0a230714b
Mitchell Brooks <mabrooks1@hotmail.co.uk>
2018-07-15 09:49:52 +0100

    Add Blocks Base

---
+++ classes/blocks/class-bones-image-text-strip.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Image/Text strip
+ *
+ * @category Blocks
+ * @package  Bones
+ * @author   Level Up Digital
+ */
+
+if ( ! class_exists( 'Bones_Image_Text_Strip' ) ) {
+
+	/**
+	 * Class Bones_Image_Text_Strip
+	 */
+	class Bones_Image_Text_Strip {
+
+		/**
+		 * Bones_Image_Text_Strip constructor.
+		 */
+		public function __construct() {
+			
+		}
+
+	}
+
+}
